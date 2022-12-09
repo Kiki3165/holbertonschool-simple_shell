2e719441731bf2ab19821dabba1b00ebbc773176
kiki3165 <kikikou82@gmail.com>
2022-12-09 13:27:26 +0000

    V0.1 without leak

--- main.c
+++ main.c
@@ -7,39 +7,25 @@
 
 int main(int ac, char **argv, char **envp)
 {
-	char *prompt = "(Eshell) $ ";
+	char *prompt = "(Eshell)$ ";
 	char *lineptr = NULL, *lineptr_copy = NULL;
 	size_t n = 0;
-	ssize_t nchars_read;
-	const char *delim = " \n";
+	/*ssize_t nchars_read;*/
+	const char *delim = "\n ";
 	int num_tokens = 0;
 	char *token;
-    char *thisEnv;
-    char *env;
 	int i;
 	/* declaring void variables */
 	(void)ac;	
 
-/* allocate space for a copy of the lineptr */
-          lineptr_copy = malloc(sizeof(char) * 1024);
-	  if (lineptr_copy== NULL)
-          {
-                  perror("tsh: memory allocation error");
-          }
-	/* Create a loop for the shell's prompt */
-	while (1) {
-		printf("%s", prompt);
-		nchars_read = getline(&lineptr, &n, stdin);
-		
-        if (nchars_read == -1)
-        {
-            printf("exiting shell....\n");
-            return (-1);
-        }
 
+	while (1) {
+		if (isatty(STDIN_FILENO))
+			printf("%s", prompt);
+		getline(&lineptr, &n, stdin);
+		
 		if(feof(stdin))
 		{
-			printf("\n");
 			break;
 		}
 		else if (strcmp("exit\n",lineptr) == 0)
@@ -48,17 +34,17 @@ int main(int ac, char **argv, char **envp)
 		}
 		else if (strcmp("env\n",lineptr) == 0)
 		{
-			for (*env = **envp; *env != 0; env++)
+			char **env;
+			for (env = envp; *env != 0; env++)
 			{
-                thisEnv = env;
+				char *thisEnv = *env;
 				printf("%s\n", thisEnv);
 			}
 		}
 	    else
 	    {
+        /* check if the getline function failed or reached EOF or user use CTRL + D */ 
 
-        /* copy lineptr to lineptr_copy */
-        strcpy(lineptr_copy, lineptr);
 
         /********** split the string (lineptr) into an array of words ********/
         /* calculate the total number of tokens */
@@ -72,11 +58,11 @@ int main(int ac, char **argv, char **envp)
         num_tokens++;
 
         /* Allocate space to hold the array of strings */
-         argv = malloc(sizeof(char *) * num_tokens);
+         argv = calloc(sizeof(char *), num_tokens);
 	if (!argv)
 		exit(1);
         /* Store each token in the argv array */
-	token = strtok(lineptr_copy, delim);
+	token = strtok(lineptr, delim);
 	
 	for (i = 0; token != NULL; i++){
 		argv[i] = token;
@@ -88,11 +74,11 @@ int main(int ac, char **argv, char **envp)
 	execmd(argv,envp);
 	
 	for (i = 0; token != NULL; i++)
-		free(argv[i]);	
-	free(argv);
+		free(argv[i]);
+        free(argv);
 	    }
     }
-	free(lineptr_copy);    
+    free(lineptr);
     /* free up allocated memory */ 
     return (0);
 }
