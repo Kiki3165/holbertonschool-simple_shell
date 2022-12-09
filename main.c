0b8595f2339f1b065bec5e595bf94948234f3156
kiki3165 <kikikou82@gmail.com>
2022-12-09 13:30:37 +0000

    No prompt without leak

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 int main(int ac, char **argv, char **envp)
 {
-	char *prompt = "(Eshell)$ ";
+	char *prompt = "";
 	char *lineptr = NULL, *lineptr_copy = NULL;
 	size_t n = 0;
 	/*ssize_t nchars_read;*/
